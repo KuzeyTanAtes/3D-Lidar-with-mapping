beeee35902c039f6e721edc252eb3c101a765884
usame_aw <osamasaedawad@gmail.com>
2023-07-14 11:31:00 +0300

    added automatic rviz visualization setup

--- threeD_mapping/launch/map_creator.launch.py
+++ threeD_mapping/launch/map_creator.launch.py
@@ -5,6 +5,10 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 
 
 def generate_launch_description():
@@ -13,6 +17,8 @@ def generate_launch_description():
     pkg_threeD = get_package_share_directory('threeD_mapping')
     slam_params_file = os.path.join(pkg_threeD, 'config', 'mapper_params_online_async.yaml')
     #slam_params_file ='/home/usame_aw/Desktop/tknls_ws/src/threeD_mapping/config/mapper_params_online_async.yaml'
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    launch_dir = os.path.join(bringup_dir, 'launch')
 
     declare_use_sim_time_argument = DeclareLaunchArgument(
         'use_sim_time',
@@ -28,10 +34,18 @@ def generate_launch_description():
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen')
+    
+    rviz_config_file = os.path.join(bringup_dir, 'rviz', 'nav2_default_view.rviz')
+    
+    rviz_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'rviz_launch.py')),
+        launch_arguments={'rviz_config': rviz_config_file}.items())
 
     ld = LaunchDescription()
 
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(start_async_slam_toolbox_node)
+    ld.add_action(rviz_cmd)
 
     return ld
