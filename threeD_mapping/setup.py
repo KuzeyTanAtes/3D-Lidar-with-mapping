3721b131850dc67f7b3b984d4be7602937e79a55
usame_aw <osamasaedawad@gmail.com>
2023-08-02 11:04:24 +0300

    added depth cam

--- threeD_mapping/setup.py
+++ threeD_mapping/setup.py
@@ -2,43 +2,30 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'threeD_mapping'
+package_name = "threeD_mapping"
 
 setup(
-
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
-
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'models'), glob('models/*.*')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "models"), glob("models/*.*")),
     ],
-    install_requires=['setuptools'],
-
+    install_requires=["setuptools"],
     zip_safe=True,
-
-    maintainer='usame_aw',
-
-    maintainer_email='osamasaedawad@gmail.com',
-
-    description='A modified turtlebot3 with some mapping functionalities',
-
-    license='TODO: License declaration',
-
-    tests_require=['pytest'],
-
+    maintainer="usame_aw",
+    maintainer_email="osamasaedawad@gmail.com",
+    description="A modified turtlebot3 with some mapping functionalities",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        
-        'rotate_node = threeD_mapping.rotate_node:main'
-            
-        ],
+        "console_scripts": ["rotate_node = threeD_mapping.rotate_node:main"],
     },
-
-
 )
--- threeD_mapping/threeD_mapping/rotate_node.py
+++ threeD_mapping/threeD_mapping/rotate_node.py
@@ -9,68 +9,76 @@ import os
 import subprocess
 import signal
 
+
 def euler_from_quaternion(x, y, z, w):
-        """
-        Convert a quaternion into euler angles (roll, pitch, yaw)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        """
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-     
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-     
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-     
-        return roll_x, pitch_y, yaw_z # in radians
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z  # in radians
 
 
 class Feedback(Node):
-
     flag = True
-    def __init__(self):
-        super().__init__('rotate_me')
-        self.declare_parameter('theta', rclpy.Parameter.Type.DOUBLE)
-        self.get_logger().info("rotate_me launched")
-        self.odomReader = self.create_subscription(Odometry, '/odom', self.sub_callback, 10)
-        self.odomReader
-        self.angularZpub = self.create_publisher(Twist, '/cmd_vel', 10)
 
+    def __init__(self):
+        super().__init__("rotate_me")
+        self.declare_parameter("theta", rclpy.Parameter.Type.DOUBLE)
+        self.get_logger().info("rotate_me launched")
+        self.odomReader = self.create_subscription(
+            Odometry, "/odom", self.sub_callback, 10
+        )
+        self.odomReader
+        self.angularZpub = self.create_publisher(Twist, "/cmd_vel", 10)
 
     def sub_callback(self, msgReceived: Odometry):
-
         msgToSend = Twist()
 
-        quat = [msgReceived.pose.pose.orientation.x, msgReceived.pose.pose.orientation.y, msgReceived.pose.pose.orientation.z, msgReceived.pose.pose.orientation.w]
+        quat = [
+            msgReceived.pose.pose.orientation.x,
+            msgReceived.pose.pose.orientation.y,
+            msgReceived.pose.pose.orientation.z,
+            msgReceived.pose.pose.orientation.w,
+        ]
         current_theta = euler_from_quaternion(quat[0], quat[1], quat[2], quat[3])
-        theta = math.radians(self.get_parameter('theta').value)
+        theta = math.radians(self.get_parameter("theta").value)
 
-        difference = current_theta[2]-theta
-        if (abs(difference) < math.radians(0.5)):
-             msgToSend.angular.z = 0.0
-             self.angularZpub.publish(msgToSend)
-             self.destroy_node()
-             rclpy.shutdown()
-        if (difference > math.radians(3)):
-             msgToSend.angular.z = -0.3
-        elif (difference < -math.radians(3)):
-             msgToSend.angular.z = 0.3  
+        difference = current_theta[2] - theta
+        if abs(difference) < math.radians(0.5):
+            msgToSend.angular.z = 0.0
+            self.angularZpub.publish(msgToSend)
+            self.destroy_node()
+            rclpy.shutdown()
+        if difference > math.radians(3):
+            msgToSend.angular.z = -0.3
+        elif difference < -math.radians(3):
+            msgToSend.angular.z = 0.3
 
         self.angularZpub.publish(msgToSend)
 
-def main(args = None):
+
+def main(args=None):
     rclpy.init(args=args)
     rotate_me = Feedback()
     rclpy.spin(rotate_me)
     rotate_me.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
